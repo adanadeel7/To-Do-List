858c2975cbee21dca55c6a60f20136928de6332a
Adan Adeel <adanadeel903@gmail.com>
2025-12-14 11:14:15 +0500

    Enter Functionality Added
    Before we could only add the tasks by pressing the add icon but now it can be done by pressing enter

--- Web Dev Projects/To_Do_List/script.js
+++ Web Dev Projects/To_Do_List/script.js
@@ -1,8 +1,8 @@
 const input = document.querySelector('.bar'); 
 const button = document.querySelector('.plus'); 
-const taskList = document.querySelector('.List'); 
+const taskList = document.querySelector('.task'); 
 
-
+// Enter Click Functionality
 button.addEventListener('click',function() { 
     const tasktext = input.value.trim(); 
     if (tasktext === "") { 
@@ -17,6 +17,27 @@ button.addEventListener('click',function() {
     li.appendChild(taskSpan);
     taskList.appendChild(li);
     input.value ='';
+    
+    
+
+});
+// Enter Functionality
+document.addEventListener('keydown',function(event) { 
+    const tasktext = input.value.trim(); 
+    if (tasktext === "") { 
+        return;
+    }
+    if (event.key === 'Enter') {
+        const li = document.createElement('li');
+        const checkbox = document.createElement('input');
+        const taskSpan = document.createElement('span');
+        checkbox.type = 'checkbox';
+        taskSpan.textContent = tasktext; 
+        li.appendChild(checkbox);
+        li.appendChild(taskSpan);
+        taskList.appendChild(li);
+        input.value ='';
+}
     
 
 });
