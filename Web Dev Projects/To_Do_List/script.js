c206c62a6032bb205cb873921156618466d50c90
Adan Adeel <adanadeel903@gmail.com>
2025-12-14 12:43:34 +0500

    Completed

--- Web Dev Projects/To_Do_List/script.js
+++ Web Dev Projects/To_Do_List/script.js
@@ -6,15 +6,19 @@ const taskList = document.querySelector('.task');
 button.addEventListener('click',function() { 
     const tasktext = input.value.trim(); 
     if (tasktext === "") { 
+        alert("Please enter a task before adding it!");
         return;
     }
     const li = document.createElement('li');
     const checkbox = document.createElement('input');
     const taskSpan = document.createElement('span');
+     const close = document.createElement('button');
     checkbox.type = 'checkbox';
+    close.textContent = 'X';
     taskSpan.textContent = tasktext; 
     li.appendChild(checkbox);
     li.appendChild(taskSpan);
+    li.appendChild(close);
     taskList.appendChild(li);
     input.value ='';
     
@@ -25,19 +29,46 @@ button.addEventListener('click',function() {
 document.addEventListener('keydown',function(event) { 
     const tasktext = input.value.trim(); 
     if (tasktext === "") { 
+        alert("Please enter a task before adding it!");
         return;
     }
     if (event.key === 'Enter') {
         const li = document.createElement('li');
         const checkbox = document.createElement('input');
         const taskSpan = document.createElement('span');
+        const close = document.createElement('button');
         checkbox.type = 'checkbox';
+        close.textContent = 'X';
         taskSpan.textContent = tasktext; 
         li.appendChild(checkbox);
         li.appendChild(taskSpan);
+        li.appendChild(close);
         taskList.appendChild(li);
+        
         input.value ='';
 }
     
 
 });
+
+
+// Deleting Functionality 
+taskList.addEventListener('click', function(e) { 
+    if (e.target.tagName === 'BUTTON' && e.target.textContent === 'X') { 
+        const ItemToDelete = e.target.closest('li'); 
+        
+        if (ItemToDelete) { 
+            ItemToDelete.remove();
+        }
+    }
+})
+
+// Strike Through
+taskList.addEventListener('change', function(e) { 
+    if (e.target.type === 'checkbox') { 
+        const LastItem = e.target.closest('li')
+
+        LastItem.classList.toggle('completed');
+    }
+})
+
